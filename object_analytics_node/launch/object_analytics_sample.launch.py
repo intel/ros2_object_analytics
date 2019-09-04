d2fc09f5260b82b1faee1e2cf2444298c5396305
challenzhou <challenzhou@gmail.com>
2019-09-04 11:23:56 +0800

    Topic align with lastest ROS2 realsense

--- object_analytics_node/launch/object_analytics_sample.launch.py
+++ object_analytics_node/launch/object_analytics_sample.launch.py
@@ -30,7 +30,7 @@ def generate_launch_description():
             remappings=[
                 ('/object_analytics/detected_objects', '/ros2_openvino_toolkit/detected_objects'),
                 ('/object_analytics/rgb', '/camera/color/image_raw'),
-                ('/object_analytics/pointcloud', '/camera/aligned_depth_to_color/color/points')],
+                ('/object_analytics/pointcloud', '/camera/pointcloud')],
             output='screen'),
 
         # object_analytics_rviz
