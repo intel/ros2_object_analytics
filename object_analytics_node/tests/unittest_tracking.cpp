7689da8b82731ee1fbd35875e600c3320db33c06
challenzhou <challenzhou@gmail.com>
2019-11-05 13:56:04 +0800

    fix tracking unit test issue

--- object_analytics_node/tests/unittest_tracking.cpp
+++ object_analytics_node/tests/unittest_tracking.cpp
@@ -19,22 +19,32 @@
 #include "tracker/tracking.hpp"
 
 TEST(UnitTestTracking, TrackingNomal) {
-  tracker::Tracking t(2, "cat", 0.7,
-    cv::Rect2d(100, 100, 200, 300));
+  cv::Mat image = cv::Mat::zeros(640, 480, CV_32F);
+  timespec stamp;
+  stamp.tv_sec = 1;
+  stamp.tv_nsec = 1;
+  std::shared_ptr<sFrame> frame = std::make_shared<sFrame>(image, stamp);
+  tracker::Tracking t(2, "cat");
+  t.rectifyTracker(frame, cv::Rect2d(100,100,200,300));
+
   EXPECT_EQ(t.getObjName(), std::string("cat"));
   EXPECT_EQ(t.getTrackingId(), int32_t(2));
-  EXPECT_NEAR(t.getObjProbability(), 0.7, 0.000001);
   EXPECT_EQ(t.getTrackedRect().x, 100);
   EXPECT_EQ(t.getTrackedRect().y, 100);
   EXPECT_EQ(t.getTrackedRect().height, 300);
   EXPECT_EQ(t.getTrackedRect().width, 200);
 }
 TEST(UnitTestTracking, TrackingNullName) {
-  tracker::Tracking t(2, "", 0.7,
-    cv::Rect2d(100, 100, 200, 300));
+  cv::Mat image = cv::Mat::zeros(640, 480, CV_32F);
+  timespec stamp;
+  stamp.tv_sec = 1;
+  stamp.tv_nsec = 1;
+  std::shared_ptr<sFrame> frame = std::make_shared<sFrame>(image, stamp);
+  tracker::Tracking t(2, "");
+  t.rectifyTracker(frame, cv::Rect2d(100,100,200,300));
+
   EXPECT_EQ(t.getObjName(), std::string(""));
   EXPECT_EQ(t.getTrackingId(), int32_t(2));
-  EXPECT_NEAR(t.getObjProbability(), 0.7, 0.000001);
   EXPECT_EQ(t.getTrackedRect().x, 100);
   EXPECT_EQ(t.getTrackedRect().y, 100);
   EXPECT_EQ(t.getTrackedRect().height, 300);
